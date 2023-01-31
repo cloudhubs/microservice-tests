7dd24f0874032d658ce123783df90404255638a8
SheldonS23 <sheldon_hssacademy@comcast.net>
2023-01-31 16:55:53 -0600

    Created navigation pages and content

--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -1,3 +1,14 @@
+import "./styles.css"
+
 export default function About() {
-    return <h1>About</h1>
+    return (<>       
+    <h1>About Our Project</h1>
+    <div className="containerMain">
+        <div className="aboutText"> 
+            This application was created by Sheldon Smith, Ethan Robinson, Timmy Frederiksen, and Trae Stevens.  This was created for CSI 43C9, the Capstone Design Course at Baylor University.
+            <br/><br/>
+            The aim of this project is to develop realistic tests for selected microservice projects and work with other teams who aim at test coverage analysis or dynamic system analysis.  Our project is centered around creating stress, load, and functional tests for OSS Microservices.  Our main goals for this project are OpenTracing Logging with possible workloads, well-defined use cases, and their implementations in Selenium and Gatling, and to create business processes reconstructed from the logs.
+        </div>   
+    </div>    
+    </>)
 }
