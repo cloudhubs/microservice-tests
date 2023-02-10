08cc31c175bd2bec504beb7475d2253dbec952d1
Ethan Robinson <ethan.robinson@yahoo.com>
2023-02-10 17:16:18 -0600

    Browse Test
    Creased a test for browsing to the next page.

---
+++ Selenium/Selenium_eShop/src/test/java/com/example/BrowsePagesItems/BrowseNext.java
@@ -0,0 +1,84 @@
+package com.example.BrowsePagesItems;
+
+import java.time.Duration;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class BrowseNext {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ethan_Robinson2\\Desktop\\Capstone-Project5\\Selenium\\chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testBrowseNext() throws Exception {
+    driver.get("http://host.docker.internal:5100/");
+    driver.findElement(By.id("Next")).click();
+    String subtitle = (String)driver.findElement(By.xpath("/html/body/div/div[2]/div/article/nav/span")).getText();
+    subtitle = (String)js.executeScript("var subtitle = \"" + subtitle + "\";var storedVars = { 'subtitle': subtitle }; return " + "\"" + subtitle + "\"" + "");
+    try {
+      assertEquals("Page 2 - 2", js.executeScript("var subtitle = \"" + subtitle + "\";var storedVars = { 'subtitle': subtitle }; return " + "\"" + subtitle + "\"" + ""));
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
