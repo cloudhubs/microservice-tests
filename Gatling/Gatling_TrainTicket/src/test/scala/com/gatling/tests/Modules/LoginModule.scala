6af2244a69b059a852e10068ace32169c1833fbc
SheldonS23 <sheldon_hssacademy@comcast.net>
2023-03-02 15:25:05 -0600

    Refactored login test to use feeders

--- Gatling/Gatling_TrainTicket/src/test/scala/com/gatling/tests/Modules/LoginModule.scala
+++ Gatling/Gatling_TrainTicket/src/test/scala/com/gatling/tests/Modules/LoginModule.scala
@@ -11,24 +11,29 @@ object LoginModule {
   //Go to home page of application
   val homePage = exec(http("Home Page")
     .get("/index.html")
-    .headers(mainPageHeader))
+    .headers(mainPageHeader)
+    .check(status.is(200)))
     .pause(2)
 
   //Go to admin login page
   val adminLoginPage = exec(http("Go to Admin Login Page")
     .get("/adminlogin.html")
-    .headers(mainPageHeader))
+    .headers(mainPageHeader)
+    .check(status.is(200)))
     .pause(3)
 
   //Complete login for admin
   val adminLogin = exec(http("Send Admin Login Request")
     .post("/api/v1/users/login")
     .headers(apiV1Header)
-    .body(RawFileBody("com/gatling/tests/${login_file}")))
+    .check(status.is(200))
+    .formParam("username", "${username}")
+    .formParam("password", "${password}"))
 
   //Go to admin home page and get needed resources
   val adminHomePage = exec(http("Go to Admin Page")
     .get("/admin.html")
+    .check(status.is(200))
     .resources(http("Get User List")
       .get("/api/v1/userservice/users")
       .headers(apiV1Header),
@@ -42,6 +47,7 @@ object LoginModule {
   //Go to main user login page
   val userLoginPage = exec(http("Go to User Login Page")
     .get("/client_login.html")
+    .check(status.is(200))
     .resources(http("Generate CAPTCHA")
       .get("/api/v1/verifycode/generate")
       .headers(apiV1Header)))
@@ -51,21 +57,22 @@ object LoginModule {
   val submitUserLogin = exec(http("Send User Login Request")
     .post("/api/v1/users/login")
     .headers(apiV1Header)
-    .body(RawFileBody("com/gatling/tests/${login_file}")))
+    .formParam("username", "${username}")
+    .formParam("password", "${password}")
+    .formParam("verificationCode", "${verification_code}")
+    .check(status.is(200)))
 
   /**Change to have feeder*/
   //Scenario to log in admin
-  val adminLoginScenario: ChainBuilder = exec { session =>
-    val newSession = session.setAll("login_file" -> "Login/admin_login.json")
-    newSession
+  val adminLoginScenario: ChainBuilder = exec {
+    feed(csv("Login/admin_login.csv").random)
+      .exec(homePage, adminLoginPage, adminLogin, adminHomePage)
   }
-  .exec(homePage, adminLoginPage, adminLogin, adminHomePage)
 
   /**Change to have feeder*/
   //Scenario to log in user
-  val userLoginScenario: ChainBuilder = exec { session =>
-    val newSession = session.setAll("login_file" -> "Login/user_login.json")
-    newSession
+  val userLoginScenario: ChainBuilder = exec {
+    feed(csv("Login/user_login.csv").random)
+      .exec(homePage, userLoginPage, submitUserLogin, homePage)
   }
-  .exec(homePage, userLoginPage, submitUserLogin)
 }
