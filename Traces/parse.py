1f1ed1d0de3a276e2d59bcd0e1d19c5a49f21fed
TheLunchTrae <xxtuberxx@gmail.com>
2023-05-05 16:15:37 -0500

    Parser now separates out unique traces. Unique traces are written individually. All traces written to unified log file

--- Traces/parse.py
+++ Traces/parse.py
@@ -31,14 +31,28 @@ def writeTraceFile(file, trace):
    caseFile = open(file, "w")
    caseFile.write(HEADER)
    caseFile.writelines(trace)
+#Sees if a given trace has a duplicate in a list of traces
+def hasDuplicate(traces, newTrace):
+   for trace in traces:
+      if len(trace) != len(newTrace):
+         continue
+      same = True
+      for eventa, eventb in zip(trace, newTrace):
+         if eventa[2:] != eventb[2:]:
+            same = False
+            break
+      if same:
+         return same 
     
 #Start of main
 if len(sys.argv) != 2:
     print("Error: Usage ./parse.py <log file>")
  
 with open(sys.argv[1], "r") as traces:
-    #TraceID, StartTime, OperationName, Source, Target
+    #Contains all traces from the original log
     allTraces = []
+    #Contains a list of unique traces. The first trace to match a case will be the one in this list
+    uniqueTraces=[]
     #Skips header
     traces.readline()
     #Parse through trace log and separate individual cases
@@ -50,6 +64,10 @@ with open(sys.argv[1], "r") as traces:
         currSet.append(trace)
       #If it is a new trace, append the current one and start a new one
       elif currSet[len(currSet)-1][TRACEID] != trace[TRACEID]:
+        #Check if the case is new before adding to the unique case list
+        if not hasDuplicate(allTraces, currSet):
+          uniqueTraces.append(currSet)
+        #Append to list of all traces
         allTraces.append(currSet)
         currSet = [ trace ]
       #If it is a new trace, add it to the current set
@@ -61,8 +79,8 @@ allEntries = []
 for traceSet in allTraces:
   allEntries = allEntries + createTraceEntries(traceSet)
 writeTraceFile("./modified_logs.csv", allEntries)
-#Write each trace to individual file
-for traceSet in allTraces:
+#Write unique traces to individual file
+for traceSet in uniqueTraces:
   fileName = "./logs/{0}.csv"
   writeTraceFile(fileName.format(traceSet[0][TRACEID]), createTraceEntries(traceSet))
     
