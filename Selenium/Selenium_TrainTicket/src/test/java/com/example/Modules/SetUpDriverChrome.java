639e29264759064b8e03b096b98d2d9d30299acf
Jorge Yero Salazar <jyero96@gmail.com>
2023-10-12 10:01:12 -0500

    Remove hardcoded chrome version

--- Selenium/Selenium_TrainTicket/src/test/java/com/example/Modules/SetUpDriverChrome.java
+++ Selenium/Selenium_TrainTicket/src/test/java/com/example/Modules/SetUpDriverChrome.java
@@ -9,26 +9,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import java.time.Duration;
 
 
 public class SetUpDriverChrome {
-    // The path to the Chrome WebDriver
-    public static final String CHROME_DRIVER = "./chromedriver.exe";
-
     /**
      * Sets up the Chrome WebDriver and returns it
      *
      * @return Chrome WebDriver
      */
-    public static Pair<WebDriver, WebDriverWait> Execute(String path) {
+    public static Pair<WebDriver, WebDriverWait> Execute() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         options.addArguments("--headless");
         options.addArguments("window-size=1920,1080");
 
-        System.setProperty("webdriver.chrome.driver", path);
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(options);
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -40,9 +39,5 @@ public class SetUpDriverChrome {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
 
         return Pair.of(driver, wait);
-    }
-
-    public static Pair<WebDriver, WebDriverWait> Execute() {
-        return SetUpDriverChrome.Execute(CHROME_DRIVER);
     }
 }
